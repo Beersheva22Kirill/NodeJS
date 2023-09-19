a1205198a18e7d7fafb32fb185f4f0bcc8ac642c
kirill2884 <kirill2884@gmail.com>
2023-09-19 14:12:34 +0300

    del files

--- src/validation/UserValidator.mjs
+++
@@ -1,7 +0,0 @@
-import Joi from "joi";
-
-export const schemaUser = Joi.object({
-    username:Joi.string().email().required(),
-    password: Joi.string().alphanum().min(6).required(),
-    roles:Joi.array().items(Joi.string().valid('admin','user')).required()
-})
